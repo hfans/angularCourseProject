d51647b4918296ad5b9d96a022240d15b89e9c62
hfans <hfans@umich.edu>
2016-04-11 19:03:59 -0400

    tied wait list form to controller so data can be submited

--- wait-and-eat/src/app/waitList/waitList.controller.js
+++ wait-and-eat/src/app/waitList/waitList.controller.js
@@ -8,5 +8,11 @@
         var vm = this;
         
         vm.parties = [1, 2, 3, 4 ];
+        
+        vm.addParty = addParty;
+        
+        function addParty() {
+            vm.parties.push('another');
+        }
     }
 })();
