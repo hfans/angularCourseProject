7ad354c739484d9461e52de30164780688ddf258
Henry Fanson <hfans@umich.edu>
2016-05-05 12:04:08 -0400

    data now adds to party
    clicking add party now adds data to the parties array

--- wait-and-eat/src/app/waitList/waitList.controller.js
+++ wait-and-eat/src/app/waitList/waitList.controller.js
@@ -4,9 +4,17 @@
         .module('app.waitList')
         .controller('WaitListController', WaitListController);
     
-    function WaitListController() {
+    WaitListController.$inject = ['$firebaseArray'];
+    
+    function WaitListController($firebaseArray) {
         var vm = this;
         
         vm.parties = [1, 2, 3, 4 ];
+        
+        vm.addParty = addParty;
+        
+        function addParty() {
+            vm.parties.push('another');
+        }
     }
 })();
