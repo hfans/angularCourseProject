8dae4740056e3c9150ebe6fb68b573e8493b19f0
Henry Fanson <hfans@umich.edu>
2016-05-10 14:08:55 -0400

    added controller content for auth

--- wait-and-eat/src/app/auth/auth.controller.js
+++ wait-and-eat/src/app/auth/auth.controller.js
@@ -5,6 +5,22 @@
         .module('app.auth')
         .controller('AuthController', AuthController);
     
-    function AuthController() {
+    AuthController.$inject = ['$firebaseAuth']
+    
+    function AuthController($firebaseAuth) {
+        var vm = this;
+        var firebaseReference = new Firebase('https://intense-fire-614.firebaseio.com');
+        var firebaseAuthObject = $firebaseAuth(firebaseReference);
+        
+        vm.user = {
+            email: '',
+            password: ''
+        }
+        
+        vm.register = register;
+        
+        function register(user){
+            return firebaseAuthObject.$createUser
+        }
     }
 })();
