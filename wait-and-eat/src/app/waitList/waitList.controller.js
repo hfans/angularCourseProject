6c013dda4a5cd9baeb57d2960533f27aec601efd
hfans <hfans@umich.edu>
2016-05-12 14:23:09 -0400

    fixed url and added more uses

--- wait-and-eat/src/app/waitList/waitList.controller.js
+++ wait-and-eat/src/app/waitList/waitList.controller.js
@@ -5,13 +5,13 @@
         .module('app.waitList')
         .controller('WaitListController', WaitListController);
     
-    WaitListController.$inject = ['$firebaseArray'];
+    WaitListController.$inject = ['$firebaseArray', 'FIREBASE_URL'];
     
-    function WaitListController($firebaseArray) {
+    function WaitListController($firebaseArray, FIREBASE_URL) {
         var vm = this;
         
-        var fireParties = new Firebase('https://intense-fire-614.firebaseio.com/parties');
-        var fireTextMessages = new Firebase('https://intense-fire-614.firebaseio.com/textMessages');
+        var fireParties = new Firebase(FIREBASE_URL + 'parties');
+        var fireTextMessages = new Firebase(FIREBASE_URL + 'textMessages');
         
         function Party() {
             this.name = '';
@@ -37,12 +37,12 @@
             vm.parties.$remove(party);
         }
         
-        function sendTextMessage(party){
+        function sendTextMessage(party) {
             var newTextMessage = {
                 phoneNumber: party.phone,
                 size: party.size,
                 name: party.name
-            }
+            };
             fireTextMessages.push(newTextMessage);
             party.notified = true;
             vm.parties.$save(party);
